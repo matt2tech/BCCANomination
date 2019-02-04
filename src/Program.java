c7f47e2a7fab0a88e93c20536aa5390e010ac284
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 16:18:38 -0600

    Added if statements for type of user

--- src/Program.java
+++ src/Program.java
@@ -9,9 +9,18 @@ public class Program {
 
     public void go() {
         String user = mainMenu();
+        if (user.equals("1")) {
+            Nomination nom = new Nomination();
+            nom.go();
+        } else if (user.equals("2")) {
+            Student stu = new Student();
+            stu.go();
+        } else {
+            System.out.println("System Error!");
+        }
     }
 
-    public String mainMenu() {
+    private String mainMenu() {
         System.out.println("(1) Nominator\t(2) Applicant");
         while (true) {
             Scanner scan = new Scanner(System.in);
