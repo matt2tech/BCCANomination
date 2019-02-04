d958ea24f6c6dbdb677721d41383061bc8fefde4
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 15:42:57 -0600

    Built getter and setter methods for all variables

--- src/Nomination.java
+++ src/Nomination.java
@@ -12,4 +12,108 @@ public class Nomination {
     String dedication;
     String workEthic;
     String notes;
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getNominator() {
+        return nominator;
+    }
+
+    public void setNominator(String nominator) {
+        this.nominator = nominator;
+    }
+
+    public String getNominatorEmail() {
+        return nominatorEmail;
+    }
+
+    public void setNominatorEmail(String nominatorEmail) {
+        this.nominatorEmail = nominatorEmail;
+    }
+
+    public String getSchoolDistrict() {
+        return schoolDistrict;
+    }
+
+    public void setSchoolDistrict(String schoolDistrict) {
+        this.schoolDistrict = schoolDistrict;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+
+    public String getStudentAge() {
+        return studentAge;
+    }
+
+    public void setStudentAge(String studentAge) {
+        this.studentAge = studentAge;
+    }
+
+    public String getStudentGraduation() {
+        return studentGraduation;
+    }
+
+    public void setStudentGraduation(String studentGraduation) {
+        this.studentGraduation = studentGraduation;
+    }
+
+    public String getAptitude() {
+        return aptitude;
+    }
+
+    public void setAptitude(String aptitude) {
+        this.aptitude = aptitude;
+    }
+
+    public String getPerseverance() {
+        return perseverance;
+    }
+
+    public void setPerseverance(String perseverance) {
+        this.perseverance = perseverance;
+    }
+
+    public String getDedication() {
+        return dedication;
+    }
+
+    public void setDedication(String dedication) {
+        this.dedication = dedication;
+    }
+
+    public String getWorkEthic() {
+        return workEthic;
+    }
+
+    public void setWorkEthic(String workEthic) {
+        this.workEthic = workEthic;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
