7c35f7721917574cf133da981ea7e443e9c644e7
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 15:04:11 -0600

    Added variables for Nomination

--- src/Nomination.java
+++ src/Nomination.java
@@ -1,2 +1,15 @@
 public class Nomination {
+    String student;
+    String nominator;
+    String nominatorEmail;
+    String schoolDistrict;
+    String position;
+    String relationship;
+    String studentAge;
+    String studentGraduation;
+    String aptitude;
+    String perseverance;
+    String dedication;
+    String workEthic;
+    String notes;
 }
