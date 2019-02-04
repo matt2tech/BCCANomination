ffa4366a56cf0a4270cd4494d53ddea80e375394
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 14:55:08 -0600

    Built mainMenu to confirm which user is using the app

--- src/Program.java
+++ src/Program.java
@@ -6,4 +6,23 @@ public class Program {
         Program app = new Program();
         app.go();
     }
+
+    public void go() {
+        String user = mainMenu();
+    }
+
+    public String mainMenu() {
+        System.out.println("(1) Nominator\t(2) Applicant");
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            String user = scan.nextLine();
+            if(user.equals("1")) {
+                return user;
+            } else if (user.equals("2")) {
+                return user;
+            } else {
+                System.out.println("Invalid!\n(1) Nominator\t(2) Applicant");
+            }
+        }
+    }
 }
