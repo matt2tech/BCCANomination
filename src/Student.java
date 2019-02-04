ae16c1841794c424d0b7b2404e973481ae0be3ad
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 15:09:54 -0600

    Added variables for Student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,13 @@
 public class Student {
+    String name;
+    String email;
+    String school;
+    String eligible;
+    String age;
+    String phoneNumber;
+    String graduationDate;
+    String plan;
+    String aptitude;
+    String dedicationWorkEthic;
+    String passion;
 }
