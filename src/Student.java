88d12d85259ed52fa6874ad7ea172740a6c0257b
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-04 15:50:00 -0600

    Built getter and setter methods for all variables

--- src/Student.java
+++ src/Student.java
@@ -10,4 +10,92 @@ public class Student {
     String aptitude;
     String dedicationWorkEthic;
     String passion;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setEligible(String eligible) {
+        this.eligible = eligible;
+    }
+
+    public String getEligible() {
+        return eligible;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setGraduationDate(String graduationDate) {
+        this.graduationDate = graduationDate;
+    }
+
+    public String getGraduationDate() {
+        return graduationDate;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setAptitude(String aptitude) {
+        this.aptitude = aptitude;
+    }
+
+    public String getAptitude() {
+        return aptitude;
+    }
+
+    public void setDedicationWorkEthic(String dedicationWorkEthic) {
+        this.dedicationWorkEthic = dedicationWorkEthic;
+    }
+
+    public String getDedicationWorkEthic() {
+        return dedicationWorkEthic;
+    }
+
+    public void setPassion(String passion) {
+        this.passion = passion;
+    }
+
+    public String getPassion() {
+        return passion;
+    }
 }
