0d84178baafc855ac09ffd5acde226f582773363
matt2tech <mlipsey@basecampcodingacademy.org>
2019-02-05 09:17:52 -0600

    Removed setters

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@ public class Student {
     String email;
     String school;
     String eligible;
-    String age;
+    Integer age;
     String phoneNumber;
     String graduationDate;
     String plan;
@@ -11,88 +11,44 @@ public class Student {
     String dedicationWorkEthic;
     String passion;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setSchool(String school) {
-        this.school = school;
-    }
-
     public String getSchool() {
         return school;
-    }
-
-    public void setEligible(String eligible) {
-        this.eligible = eligible;
     }
 
     public String getEligible() {
         return eligible;
     }
 
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getAge() {
+    public Integer getAge() {
         return age;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setGraduationDate(String graduationDate) {
-        this.graduationDate = graduationDate;
-    }
-
     public String getGraduationDate() {
         return graduationDate;
-    }
-
-    public void setPlan(String plan) {
-        this.plan = plan;
     }
 
     public String getPlan() {
         return plan;
     }
 
-    public void setAptitude(String aptitude) {
-        this.aptitude = aptitude;
-    }
-
     public String getAptitude() {
         return aptitude;
     }
 
-    public void setDedicationWorkEthic(String dedicationWorkEthic) {
-        this.dedicationWorkEthic = dedicationWorkEthic;
-    }
-
     public String getDedicationWorkEthic() {
         return dedicationWorkEthic;
-    }
-
-    public void setPassion(String passion) {
-        this.passion = passion;
     }
 
     public String getPassion() {
